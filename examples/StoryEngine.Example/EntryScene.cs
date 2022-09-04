07ce1dec0ec3456d7293dddc66a4afdd6b2f77e7
Piotr Paszko <piotr.paszko.dev@gmail.com>
2022-09-04 21:23:54 +0200

    Implement new example project

--- examples/StoryEngine.Example/EntryScene.cs
+++ examples/StoryEngine.Example/EntryScene.cs
@@ -5,114 +5,66 @@ using StoryEngine.Core.Input;
 
 namespace StoryEngine.Example
 {
-    public enum EnableState
-    {
-        AllEnabled,
-        SceneOneDisabled,
-        SceneTwoDisabled,
-        AllDisabled
-    }
-
     public class EntryScene : IScene
     {
-        private readonly IScenesManager _scenesManager;
+        public int Layer { get; } = 0;
+
         private readonly IWindow _window;
-        private readonly IInputReader _keyReader;
         private readonly IButtonHandler _buttonHandler;
-        private readonly int _displayHeight;
+        private readonly IScenesManager _scenesManager;
 
-        private SceneOne? _sceneOne;
-        private SceneTwo? _sceneTwo;
+        private readonly Button _scenesExampleButton = new Button(ConsoleKey.A);
+        private readonly Button _selectionListExampleButton = new Button(ConsoleKey.B);
 
-        private readonly Button _changeSceneButton = new Button(ConsoleKey.Spacebar);
-        private readonly Button _changeEnableStateButton = new Button(ConsoleKey.W);
+        private readonly Text Text = new Text("Press A for scenes example.\nPress B for selection list example.",
+            Coordinates.Zero);
 
-        private EnableState _enableState = EnableState.AllEnabled;
+        private readonly Coordinates _delayTextCoordinates;
+
+        private bool _enabled = true;
 
         public EntryScene(
-            IScenesManager scenesManager,
             IWindow window,
-            IInputReader keyReader,
             IButtonHandler buttonHandler,
-            EngineConfiguration configuration)
+            IScenesManager scenesManager,
+            EngineConfiguration engineConfiguration)
         {
-            _scenesManager = scenesManager;
             _window = window;
-            _keyReader = keyReader;
             _buttonHandler = buttonHandler;
-            _displayHeight = configuration.WindowSize.Height - 1;
-        }
+            _scenesManager = scenesManager;
 
-        public int Layer { get; } = 0;
+            _delayTextCoordinates = new Coordinates(0, engineConfiguration.WindowSize.Height - 1);
+        }
 
         public void Initialize()
         {
-            _sceneOne = _scenesManager.LoadScene<SceneOne>();
-            _sceneTwo = _scenesManager.LoadScene<SceneTwo>();
         }
 
         public void Update(DeltaTime deltaTime)
         {
-            _window.Draw(new Text("Press Space or W :)", new Coordinates(0, _displayHeight - 1)));
-            _window.Draw(new Text($"Delay: {deltaTime.TimeElapsed.Milliseconds}ms.", new Coordinates(0, _displayHeight)));
-
-            if(_buttonHandler.Update(_changeSceneButton))
+            if(_enabled)
             {
-                SwitchScenesLayers();
+                _window.Draw(Text);
+
+                if (_buttonHandler.Update(_scenesExampleButton))
+                {
+                    _scenesManager.LoadScene<ScenesExampleScene>();
+                    DisableCurrentSceneText();
+                }
+
+                if (_buttonHandler.Update(_selectionListExampleButton))
+                {
+                    _scenesManager.LoadScene<SelectionListScene>();
+                    DisableCurrentSceneText();
+                }
             }
 
-            if(_buttonHandler.Update(_changeEnableStateButton))
-            {
-                IncrementEnableState();
-                ApplyEnableState();
-            }
+            _window.Draw(new Text($"Delay: {deltaTime.TimeElapsed.Milliseconds}ms.", _delayTextCoordinates));
         }
 
-        private void SwitchScenesLayers()
+        private void DisableCurrentSceneText()
         {
-            var sceneOneLayer = _sceneOne!.Layer;
-            _sceneOne.Layer = _sceneTwo!.Layer;
-            _sceneTwo.Layer = sceneOneLayer;
-        }
-
-        private void IncrementEnableState()
-        {
-            if (_enableState == EnableState.AllDisabled)
-                _enableState = EnableState.AllEnabled;
-            else
-                _enableState++;
-        }
-
-        private void ApplyEnableState()
-        {
-            switch(_enableState)
-            {
-                case EnableState.AllEnabled:
-                    EnableScene<SceneOne>(true);
-                    EnableScene<SceneTwo>(true);
-                    break;
-
-                case EnableState.SceneOneDisabled:
-                    EnableScene<SceneOne>(false);
-                    EnableScene<SceneTwo>(true);
-                    break;
-
-                case EnableState.SceneTwoDisabled:
-                    EnableScene<SceneOne>(true);
-                    EnableScene<SceneTwo>(false);
-                    break;
-
-                case EnableState.AllDisabled:
-                    EnableScene<SceneOne>(false);
-                    EnableScene<SceneTwo>(false);
-                    break;
-            }
-        }
-
-        private void EnableScene<TScene>(bool enabled) where TScene : IScene
-        {
-            var loadedScene = _scenesManager.GetLoadedScene<TScene>();
-            loadedScene.Enabled = enabled;
+            _enabled = false;
         }
     }
 }
---
+++ examples/StoryEngine.Example/SelectionListScene.cs
@@ -0,0 +1,71 @@
+﻿using StoryEngine.Core;
+using StoryEngine.Core.Components.SelectionList;
+using StoryEngine.Core.Graphics;
+using StoryEngine.Core.Input;
+
+namespace StoryEngine.Example
+{
+    public class SelectionListScene : IScene
+    {
+        public int Layer { get; set; } = 0;
+
+        private readonly IWindow _window;
+
+        private readonly SelectionListComponent _selectionListComponent;
+
+        private readonly ListElement _testElement1 = new ListElement("Test element 1");
+        private readonly ListElement _testElement2 = new ListElement("Test element 2");
+        private readonly ListElement _testElement3 = new ListElement("Test element 3");
+
+        private readonly Text _text1 = new Text("Text 1", new Coordinates(1, 10));
+        private readonly Text _text2 = new Text("Text 2", new Coordinates(1, 11));
+        private readonly Text _text3 = new Text("Text 3", new Coordinates(1, 12));
+
+        private bool _showText1 = false;
+        private bool _showText2 = false;
+        private bool _showText3 = false;
+
+        public SelectionListScene(
+            IButtonHandler buttonHandler,
+            IWindow window)
+        {
+            _selectionListComponent = new SelectionListComponent(buttonHandler, window);
+            _window = window;
+        }
+
+        public void Initialize()
+        {
+            _selectionListComponent.Coordinates = new Coordinates(5, 1);
+            _selectionListComponent.AddElement(_testElement1);
+            _selectionListComponent.AddElement(_testElement2);
+            _selectionListComponent.AddElement(_testElement3);
+            _selectionListComponent.SelectionMarker = "->";
+        }
+
+        public void Update(DeltaTime deltaTime)
+        {
+            var selected = _selectionListComponent.Update();
+
+            if(selected is not null)
+            {
+                if (selected == _testElement1)
+                    _showText1 = !_showText1;
+
+                if (selected == _testElement2)
+                    _showText2 = !_showText2;
+
+                if (selected == _testElement3)
+                    _showText3 = !_showText3;
+            }
+
+            if (_showText1)
+                _window.Draw(_text1);
+
+            if (_showText2)
+                _window.Draw(_text2);
+
+            if (_showText3)
+                _window.Draw(_text3);
+        }
+    }
+}
